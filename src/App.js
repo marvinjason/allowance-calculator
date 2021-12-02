0fc517709fcb23daba319f18472ec81d555de4ca
marvinjason <marvinjason.sy@gmail.com>
2021-12-03 02:51:35 +0800

    Create simple box with computation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
+  const [rent, setRent] = useState(0);
+  const allowance = 10000 - rent + 25;
+  const formatter = new Intl.NumberFormat('en-PH', {
+    style: 'currency',
+    currency: 'PHP'
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div className={styles.row}>
+        <p className={styles.label}>Latest paid rent <small>(inclusive of transfer fee)</small></p>
+        <input type="number" className={styles.input} onChange={e => setRent(e.target.value)} />
+        <p className={`${styles.label} ${styles['margin-top']}`}>Allowance to transfer</p>
+        <span className={styles.span}>{rent && formatter.format(allowance)}</span>
+      </div>
     </div>
   );
 }
